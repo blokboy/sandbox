a2ccf57ad5085fc32d89353358c8193efa25ed07
Jordan Marsaw <37964165+blokboy@users.noreply.github.com>
2018-08-16 00:01:44 -0500

    232792560

---
+++ ProjectEuler/ProjectEuler5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+  unsigned long result, i = 0;
+
+  for (result = 2520;; result += 2520) {
+    for (i = 10; i < 20; ++i) {
+      if (result % i) { break; }
+    }
+    if (i == 20) { break; }
+  }
+
+  printf("%lu\n", result);
+
+  return result;
+}
