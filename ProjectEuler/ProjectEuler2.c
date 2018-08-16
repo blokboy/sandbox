4348d0dfdf0d92e70f97e5957bc5599fea0fe635
Jordan Marsaw <37964165+blokboy@users.noreply.github.com>
2018-08-15 23:22:17 -0500

    4613732

---
+++ ProjectEuler/ProjectEuler2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+  int a = 0, b = 1, c = 1, sum = 0;
+
+  while (c <= 4000000) {
+    if (c % 2 == 0) { sum += c; }
+    a = b;
+    b = c;
+    c = a + b;
+  }
+
+  printf("%d\n", sum);
+
+  return sum;
+}
+
